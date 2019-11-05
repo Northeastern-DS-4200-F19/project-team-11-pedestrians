912e14b14bdb2f8e82fd8997a6cb30f5d0b61f30
ericjichung <ericjichung@gmail.com>
2019-11-05 12:03:52 -0800

    csv to array:

--- visualization.js
+++ visualization.js
@@ -1,9 +1,18 @@
 console.log('hello world!');
 
-var dataset = [];
+var dataset = []
 
-d3.csv("/data/ChesterSquareSurveyResponses.csv").then(function(data) {
-  console.log("hi");
+d3.csv("./data/ChesterSquareSurveyResponses.csv").then(function(data){
+  var time = []
+  var safety = []
+  console.log(data)
+  for(let i = 0; i < data.length; i ++) {
+    time.push(data[i]["Visit-Time"]);
+    safety.push(data[i]["Safety-Level"]);
+    // time.push(data[i][3])
+    // safety.push(data[i][6])
+  }
+  dataset.push(time)
+  dataset.push(safety)
 });
 
-console.log(dataset);
