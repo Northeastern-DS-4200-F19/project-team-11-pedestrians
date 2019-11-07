bbb472d3fa2f1860a944b2801be203f245c5c857
ericjichung <ericjichung@gmail.com>
2019-11-07 05:17:59 -0800

    init

---
+++ geoViz.js
@@ -0,0 +1,81 @@
+// NOTE: Sample code from class was used on this assignment
+// We plan to add it to the Acknowledgements section but do not know the format
+var minSafetyLevel = 0;
+var maxSafetyLevel = 10;
+var width  = 700;
+var height = 500;
+var margin = {
+  top: 30,
+  bottom: 30,
+  left: 50,
+  right: 30
+};
+
+
+const graphHeight = height - margin.top - margin.bottom;
+const graphWidth = width - margin.left - margin.right;
+
+const svg = d3.select("svg")
+const graph = svg.append("g").attr("height",graphHeight).attr("width",graphWidth)
+
+d3.csv("./data/ChesterSquareSurveyResponses.csv", function(d) {
+  return {
+    visittime: d.visittime,
+    safetylevel: + d.safetylevel
+  };
+}).then(() => {
+  geoChart
+  lineChart
+
+});
+// const geoChart = (data) => {
+
+// }
+
+function lineChart(data){
+  console.log(data);
+
+
+  var svg = d3.select('body')
+              .append('svg')
+              .attr('width' , width)
+              .attr('height', height)
+              .style('background', '#efefef');
+
+var chartGroup = svg.append('g')
+  					  .append('svg')
+                      .attr('transform','translate(' + margin.left +',' + margin.top + ')');
+
+  var xScale = d3.scaleOrdinal()
+                 .domain(["", "Morning", "Afternoon", "Evening"])
+                 // Shifting by 50 so the last category label doesn't get cut off
+                 .range([0, 100, width/2, width - 100]);
+
+  var yScale = d3.scaleLinear()
+                 .domain([minSafetyLevel, maxSafetyLevel])
+                 .range([height - margin.bottom - margin.top, 0]);
+
+  var xAxis = d3.axisBottom(xScale);
+
+  chartGroup.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate('+ margin.left+', ' + (height - margin.bottom) + ')')
+            .call(xAxis);
+
+  var yAxis = d3.axisLeft(yScale);
+
+  chartGroup.append('g')
+            .attr('class', 'y axis')
+            .attr('transform', 'translate('+ margin.left +', ' + margin.top+')')
+            .call(yAxis);
+
+  svg.append('g')
+     .selectAll("dot")
+     .data(data)
+     .enter()
+     .append("circle")
+     .attr("cx", function (d) { return xScale(d.visittime) + margin.left; } )
+     .attr("cy", function (d) { return yScale(d.safetylevel) + margin.top; } )
+     .attr("r", 5)
+     .style("fill", "#69b3a2")
+}
