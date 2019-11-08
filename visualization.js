fd4be82a1687b709f42401c5300eed465882f185
Edward Chung <EdwardC@Edwards-MacBook-Pro.local>
2019-11-08 13:51:07 -0500

    e

--- visualization.js
+++ visualization.js
@@ -1,85 +1,72 @@
-  
-console.log('hello world!');
+// NOTE: Sample code from class was used on this assignment
+// We plan to add it to the Acknowledgements section but do not know the format.
 
-var dataset = []
-
-d3.csv("./data/ChesterSquareSurveyResponses.csv").then(function(data){
-  var time = []
-  var safety = []
-  console.log(data)
-  for(let i = 0; i < data.length; i ++) {
-    var row = []
-
-    var visittime = data[i]["Visit-Time"];
-
-    //row.push(data[i]["Visit-Time"]);
-    row.push(parseInt(data[i]["Safety-Level"]));
-    // time.push(data[i][3])
-    // safety.push(data[i][6])
-    dataset.push(row)
-	}
-	console.log(dataset)
+d3.csv("./data/ChesterSquareSurveyResponses.csv", function(d) {
+  return {
+    visittime: d.visittime,
+    safetylevel: + d.safetylevel
+  };
+  console.log(d)
 }).then(lineChart);
 
 function lineChart(data){
   console.log(data);
-  var maxTime  = 2;
-  var minTime  = 0;
-  var maxSafety = 10;
 
-  var width  = 600;
+  var minSafetyLevel = 0;
+  var maxSafetyLevel = 10;
+
+  var width  = 700;
   var height = 500;
   var margin = {
     top: 30,
     bottom: 30,
-    left: 30,
+    left: 50,
     right: 30
   };
 
   var svg = d3.select('body')
-  .append('svg')
-    .attr('width' , width)
-    .attr('height', height)
-    .style('background', '#efefef');
+              .append('svg')
+              .attr('width' , width)
+              .attr('height', height)
+              .style('background', '#efefef');
 
-  var chartGroup = svg
-  .append('g')
-    .attr('transform','translate(' + margin.left +',' + margin.top + ')');
+var chartGroup = svg.append('g')
+              .append('svg')
+                      .attr('transform','translate(' + margin.left +',' + margin.top + ')');
 
   var xScale = d3.scaleOrdinal()
-  .domain(["Morning", "Afternoon", "Evening"])
-  .range([0, width]);
+                 .domain(["", "Morning", "Afternoon", "Evening"])
+                 // Shifting by 50 so the last category label doesn't get cut off
+                 .range([0, 100, width/2, width - 100]);
 
   var yScale = d3.scaleLinear()
-  	.domain([0, maxSafety])
-  	.range([height - margin.bottom - margin.top, 0]);
+                 .domain([minSafetyLevel, maxSafetyLevel])
+                 .range([height - margin.bottom - margin.top, 0]);
 
-  
   var xAxis = d3.axisBottom(xScale);
-	chartGroup.append('g')
-  	.attr('class', 'x axis')
-  	.attr('transform', 'translate(0, ' + (height - margin.bottom - margin.top) + ')')
-  	.call(xAxis);
 
+  chartGroup.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate('+ margin.left+', ' + (height - margin.bottom) + ')')
+            .call(xAxis);
 
   var yAxis = d3.axisLeft(yScale);
-	chartGroup.append('g')
-  	.attr('class', 'y axis')
-  	.attr('transform', 'translate(0, 0)')
-  	.call(yAxis);
 
-  // chartGroup.append('path')
-  //   .attr('d', line(data))
-  //   .attr('class', 'dataLine');
+  chartGroup.append('g')
+            .attr('class', 'y axis')
+            .attr('transform', 'translate('+ margin.left +', ' + margin.top+')')
+            .call(yAxis);
+
+  svg.append('g')
+     .selectAll("dot")
+     .data(data)
+     .enter()
+     .append("circle")
+     .attr("cx", function (d) { return xScale(d.visittime) + margin.left; } )
+     .attr("cy", function (d) { return yScale(d.safetylevel) + margin.top; } )
+     .attr("r", 5)
+     .style("fill", "#69b3a2")
 }
-
-
-
-
-
-
-
-
 
 
 
