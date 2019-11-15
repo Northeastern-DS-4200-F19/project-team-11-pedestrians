31266172429322f65e05c0f0acb04a4ff7ff7e89
Edward Chung <EdwardC@Edwards-MacBook-Pro.local>
2019-11-15 12:14:20 -0500

    fix final

--- data/aggregateByCategory.py
+++ data/aggregateByCategory.py
@@ -1,7 +1,9 @@
 import pandas as pd
 df = pd.read_csv("../data/crimeV3.csv", low_memory=False, usecols=["neighborhoods","offense_type","id"])
-aggregate = df.groupby(["neighborhoods","offense_type"]).agg('count')
-agg2 = aggregate.groupby("neighborhoods",as_index=False).apply(lambda x:x.nlargest(5,'id'))
-# agg2.columns=["neighborhoods","offense_type","count"]
-agg2.rename(columns={'id':'count'}, inplace=True)
+df.rename(columns={'id':'count'}, inplace=True)
+aggregate = df.groupby(["neighborhoods","offense_type"]).agg('count');
+#agg2 = aggregate.groupby("offense_type")["count"].sum().nlargest(n = 5)
+agg2 = aggregate.groupby("neighborhoods",as_index=False).apply(lambda x:x.nlargest(5,'count'))
+
+print(agg2);
 agg2.to_csv("./crimeV4.csv")
--- stackedBar.js
+++ stackedBar.js
@@ -1,14 +1,13 @@
 d3.csv("./data/crimeV4.csv", function(d) {
-	return {
-		neighborNames: d.neighborhoods,
-		crimeCount: + d["properties"]["crime"]
-	};
-	console.log(d)
+  return {
+    neighborhoods: d.neighborhoods,
+    crimeCount: + d.count,
+    crimeCategory: d.offense_type
+  };
+  console.log(d)
 }).then(stackedBar);
 
-
 function stackedBar(data){
-  console.log(data);
   var width  = 700;
   var height = 500;
   var margin = {
@@ -18,27 +17,22 @@ function stackedBar(data){
     right: 30
   };
 
-  var minCrime = 0;
-  var maxCrime  = d3.max(data, function(d){return d["properties"]["crime"]});
-
   var svg = d3.select('#stackedBar')
               .append('svg')
               .attr('width' , width)
               .attr('height', height)
               .style('background', '#efefef');
-
   var chartGroup = svg.append('g')
-  					          .append('svg')
+                      .append('svg')
                       .attr('transform','translate(' + margin.left +',' + margin.top + ')');
-
   var xScale = d3.scaleOrdinal()
-                 .domain(d["properties"]["Name"])
+                 .domain(["", "Allston", "Roxbury", "Dorchester"])
+                 // Shifting by 50 so the last category label doesn't get cut off
                  .range([0, 100, width/2, width - 100]);
 
   var yScale = d3.scaleLinear()
-                 .domain([minCrime, maxCrime + 1000])
+                 .domain([0, d3.max(data, function(d){return d.count;})])
                  .range([height - margin.bottom - margin.top, 0]);
-
   var xAxis = d3.axisBottom(xScale);
 
   chartGroup.append('g')
@@ -52,13 +46,3 @@ function stackedBar(data){
             .attr('class', 'y axis')
             .attr('transform', 'translate('+ margin.left +', ' + margin.top+')')
             .call(yAxis);
-
-  chartGroup.append("path")
-            .datum(data)
-            .attr("fill", "none")
-            .attr("stroke", "#69b3a2")
-            .attr("stroke-width", 1.5)
-            .attr("d", d3.line()
-                     .x(function (d) { return xScale(d["properties"]["Name"]) + margin.left; })
-                     .y(function (d) { return yScale(d["properties"]["crime"]) + margin.top; } ));
-}
