a24b12b45dfc38e352816b37b3f228044a81e960
ericjichung <ericjichung@gmail.com>
2019-11-14 21:39:07 -0800

    reimplemented brushing

--- geoViz.js
+++ geoViz.js
@@ -10,15 +10,7 @@ var colors = {"crime":"red","real_estate":"green"}
 const centerBoston = [-71.057,42.313]
 const graphHeight = height - margin.top - margin.bottom;
 const graphWidth = width - margin.left - margin.right;
-<<<<<<< HEAD
-<<<<<<< HEAD
-var state = 'crime'
-=======
 var state = {view:'crime',neighborhood:""};
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
-=======
-var state = {view:'crime',neighborhood:""};
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
 const graph = d3.select("#vis-svg")
               .attr("height",graphHeight)
               .attr("width",graphWidth)
@@ -39,53 +31,20 @@ const update = (info) => {
   var tip = d3.tip()
             .attr("class","tip")
             .html(d => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-              return `<p class="tool"><span>${d["properties"]["Name"]}</span><br>Total ${state}: ${Math.round(d["properties"][state])}</p>`
-            });
-    graph.call(tip);
-  //TODO convert to per feet not raw values
-  var stuff = info["features"].map(d => Math.round(d["properties"][state]))
-  var daMax = Math.max(...stuff);
-  var color = d3.scaleLinear().domain([0,daMax]).range(["white",colors[state]])
-=======
               return `<p class="tool"><span>${d["properties"]["Name"]}</span><br>Total ${state["view"]}: ${Math.round(d["properties"][state["view"]])}</p>`
             });
-    graph.call(tip);
   //TODO convert to per feet not raw values
   var stuff = info["features"].map(d => Math.round(d["properties"][state["view"]]))
   var daMax = Math.max(...stuff);
   var color = d3.scaleLinear().domain([0,daMax]).range(["white",colors[state["view"]]])
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
-=======
-              return `<p class="tool"><span>${d["properties"]["Name"]}</span><br>Total ${state["view"]}: ${Math.round(d["properties"][state["view"]])}</p>`
-            });
-    graph.call(tip);
-  //TODO convert to per feet not raw values
-  var stuff = info["features"].map(d => Math.round(d["properties"][state["view"]]))
-  var daMax = Math.max(...stuff);
-  var color = d3.scaleLinear().domain([0,daMax]).range(["white",colors[state["view"]]])
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
   var paths = graph.selectAll("path").data(info.features)
-
+  graph.call(tip);
     //updating stuff
     paths.attr('d', geoGenerator)
           .attr("fill",d => {
-<<<<<<< HEAD
-<<<<<<< HEAD
               return color(d["properties"][state])
             })
           .attr("stroke","grey")
-          .attr("stroke-width",1)
-=======
-              return color(d["properties"][state["view"]])
-            })
-          .attr("stroke","grey")
-=======
-              return color(d["properties"][state["view"]])
-            })
-          .attr("stroke","grey")
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
           .attr("stroke-width",(d) => {
             if(d["properties"]["Name"] === state["neighborhood"]) {
               return 3
@@ -93,60 +52,18 @@ const update = (info) => {
               return 1
             }
           })
-<<<<<<< HEAD
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
-=======
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
           .attr("class","neighborhoods")
           .on("mouseover",function(d){show(d,this)})
           .on("mouseout",function(d){hide(d,this)})
 
     //removing stuff
     paths.exit().remove();
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-    //removing stuff
-    paths.exit().remove();
-
-=======
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
-    //adding stuff
-    paths.enter()
-          .append('path')
-          .attr('d', geoGenerator)
-          .attr("fill",d => {
-<<<<<<< HEAD
-            return color(d["properties"][state])})
-          .attr("stroke","grey")
-          .attr("stroke-width",1)
-          .on("mouseover",tip.show)
-          .on("mouseout",tip.hide)
-
-    // var labels = graph.selectAll(".labels")
-    //       .data(data.features)
-    //       .enter()
-    //       .append('text')
-    //       .attr("transform", (d) => {
-    //         return `translate(${geoGenerator.centroid(d)})`
-    //       })
-    //       .text(d => {
-    //         return d.properties["Name"];
-    //       })
-    //       .attr("text-anchor","end")
-    //       .attr("fill","white")
-    //       .attr("font-size","8")
-    //       .attr("class","labels");
-    
-=======
-=======
 
     //adding stuff
     paths.enter()
           .append('path')
           .attr('d', geoGenerator)
           .attr("fill",d => {
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
             return color(d["properties"][state["view"]])})
           .attr("stroke",(d) => {
             console.log(d["properties"]["Name"])
@@ -178,10 +95,6 @@ const update = (info) => {
       tip.hide(d,target);
       d3.select(target).attr("stroke","grey");
     };
-<<<<<<< HEAD
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
-=======
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
 }
 
 //load all datasets here
@@ -193,16 +106,6 @@ const letsGo = (d) => {
   update(d[0]);
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-Promise.all(promises).then((d) => letsGo(d));
-=======
 const render = () => Promise.all(promises).then((d) => letsGo(d));
 
 render()
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
-=======
-const render = () => Promise.all(promises).then((d) => letsGo(d));
-
-render()
->>>>>>> 91828fdb072333220450f58d900517b19ebc4e14
