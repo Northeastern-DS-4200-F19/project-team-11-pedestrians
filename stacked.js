e9195d849833b672fb750f6acaf343564793832b
Karman Singh <karmansingh@icloud.com>
2019-11-20 11:21:50 -0500

    Fixing axes for Stacked Graph

--- stacked.js
+++ stacked.js
@@ -2,10 +2,10 @@
   //   var maxSafetyLevel = d3.max(data, function(d){ return d.value;});
   //
   var width  = 1000;
-  var height = 300;
+  var height = 600;
     var margin = {
       top: 30,
-      bottom: 10,
+      bottom: 115,
       left: 50,
       right: 75
     };
@@ -55,12 +55,12 @@ function stackChart(deets){
   var xScale = d3.scaleBand()
                  .domain(neighborhoods)
                  // Shifting by 50 so the last category label doesn't get cut off
-                 .range([0, width - margin.right - 100]);
+                 .range([0, width - margin.right - 30]);
   //
     var yScale = d3.scaleLinear()
                    .domain([0, 100])
-                   .range([height, margin.top]);
-  
+                   .range([height - margin.bottom - margin.top, margin.top]);
+
     var z = d3.scaleOrdinal(d3.schemeAccent)
                     // .range(["red","orange","yellow","green","blue","purple","indigo","white","black","grey","navy","indigo","brown","maroon"])
                    .domain([...offenses]);
@@ -69,8 +69,8 @@ function stackChart(deets){
   //
 
   var yAxis = d3.axisLeft(yScale);
-  
-  
+
+
   chartGroup.append('g')
             .attr('class', 'x_axis')
             .attr('transform', 'translate('+ margin.left+', ' + (height - margin.bottom) + ')')
@@ -93,7 +93,7 @@ function stackChart(deets){
                 // .merge(group)
                 .classed("layer",true)
                 .attr("fill", d => z(d.key))
-      
+
                 // .attr("class", "derp")
 
     group.exit().remove()
@@ -104,9 +104,9 @@ function stackChart(deets){
         .append("rect")
         .merge(bars)
         .attr("class","derp")
-        .attr("x", function (d) { return xScale(d.data.neighborhood) + margin.left + 3;})
+        .attr("x", function (d) { return xScale(d.data.neighborhood) + margin.left + 20;})
         .attr("width",xScale.bandwidth() - 3)
-        .attr("y", function (d) { return yScale(d[1]) - margin.bottom})
+        .attr("y", function (d) { return yScale(d[1])+margin.top})
         .attr("height", function (d) {return yScale(d[0]) - yScale(d[1])})
-        .attr("stroke","white")   
+        .attr("stroke","white")
   }
