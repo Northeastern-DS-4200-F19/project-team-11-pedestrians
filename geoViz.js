5bcec960ffec6159793c16ca67d8a551c9c7a1f9
Edward Chung <EdwardC@Edwards-MacBook-Pro.local>
2019-12-06 20:11:38 -0500

    fix

--- geoViz.js
+++ geoViz.js
@@ -58,7 +58,6 @@ const update = (deets) => {
             .style("font-size", "16px")
             .style("text-decoration", "underline")
             .text(title);
-  tablevis(deets, stuff);
 
   var tip = d3.tip()
             .attr("class","tipVar")
