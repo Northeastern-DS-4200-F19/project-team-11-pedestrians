de201383517efb23c8e055b796783ebc9eda5e5c
Karman Singh <karmansingh@icloud.com>
2019-12-08 16:47:58 -0500

    Speed up

--- index.js
+++ index.js
@@ -1,219 +1,268 @@
-const stateBttns = document.querySelectorAll(".btn")
-var state = {scope:"boston",view:'crime',neighborhood:[], data: {"crime": null,"demographic": null,"real_estate":null, "survey":null }, set setN(x) {
-  this.neighborhood = Array.from(x);
-  updateViz()
-}, set removeN(x) {
-  this.neighborhood = []
-  updateViz()
-},colors:{"crime":"red","real_estate":"green","demographic":"blue", "chester_square":"orange"},
+const stateBttns = document.querySelectorAll(".btn");
+var state = {
+  scope: "boston",
+  view: "crime",
+  neighborhood: [],
+  data: { crime: null, demographic: null, real_estate: null, survey: null },
+  set setN(x) {
+    this.neighborhood = Array.from(x);
+    updateViz();
+  },
+  set removeN(x) {
+    this.neighborhood = [];
+    updateViz();
+  },
+  colors: {
+    crime: "red",
+    real_estate: "green",
+    demographic: "blue",
+    chester_square: "orange"
+  },
   set setView(x) {
     this.view = x;
     //updateViz();
+    const linedata = filterLine(state.data[state.view]);
     removeChart();
-    lineChart(filterLine(state.data[state.view]));
+    lineChart(linedata);
     stackChart(filterBar(state.data[state.view]));
-    geoViz({"data":state.data[state.view],"info":state.data["geo"]});
-    tablevis(filterLine(state.data[state.view]),state.data[state.view].filter(a => a.neighborhood == "Chester Square").map(s => s.value), new Set(state.data[state.view].map(s => s.neighborhood)));
-  }};
+    geoViz({ data: state.data[state.view], info: state.data["geo"] });
+    tablevis(
+      linedata,
+      state.data[state.view]
+        .filter(a => a.neighborhood == "Chester Square")
+        .map(s => s.value),
+      new Set(state.data[state.view].map(s => s.neighborhood))
+    );
+  }
+};
 
 const removeChart = () => {
-  d3.selectAll(".stuff").remove()
-  d3.selectAll(".derp").remove()
-  d3.selectAll(".x_axis").remove()
-  d3.selectAll(".y_axis").remove()
-  d3.selectAll(".header").remove()
-  d3.selectAll(".row").remove()
-  d3.selectAll(".column").remove()
-  d3.selectAll(".thead").remove()
-  d3.selectAll(".tbody").remove()
+  d3.selectAll(".stuff").remove();
+  d3.selectAll(".derp").remove();
+  d3.selectAll(".x_axis").remove();
+  d3.selectAll(".y_axis").remove();
+  d3.selectAll(".header").remove();
+  d3.selectAll(".row").remove();
+  d3.selectAll(".column").remove();
+  d3.selectAll(".thead").remove();
+  d3.selectAll(".tbody").remove();
   // d3.select("#table").remove()
-}
+};
 
 var lineIndex = 0;
-var db = []
+var db = [];
 //load all datasets here
 var promises = [
-        d3.csv("./data/csv_files/crime.csv",function(d){
-                return {
-                            time: parseInt(d.time),
-                            value: +d.value,
-                            neighborhood: d.neighborhood,
-                            category: d.category
-                          };
-                        }),
-        d3.csv("./data/csv_files/real_estate.csv",function(d){
-          return {
-                      time: d.time,
-                      value: +d.value,
-                      neighborhood: d.neighborhood,
-                      category: d.category
-                    };
-                  }),
-        d3.csv("./data/csv_files/demographic.csv",function(d){
-          return {
-                      time: d.time,
-                      value: +d.value,
-                      neighborhood: d.neighborhood,
-                      category: d.category
-                    };
-                  }),
-        d3.csv("./data/csv_files/ChesterSquareSurveyResponses.csv",function(d) {
-                    return {
-                      visittime: d.visittime,
-                      safetylevel: + d.safetylevel,
-                      hour: parseInt(d.hour,10)
-                    }}),
-        d3.json("./data/json_files/boston.geojson")
-    ]
+  d3.csv("./data/csv_files/crime.csv", function(d) {
+    return {
+      time: parseInt(d.time),
+      value: +d.value,
+      neighborhood: d.neighborhood,
+      category: d.category
+    };
+  }),
+  d3.csv("./data/csv_files/real_estate.csv", function(d) {
+    return {
+      time: d.time,
+      value: +d.value,
+      neighborhood: d.neighborhood,
+      category: d.category
+    };
+  }),
+  d3.csv("./data/csv_files/demographic.csv", function(d) {
+    return {
+      time: d.time,
+      value: +d.value,
+      neighborhood: d.neighborhood,
+      category: d.category
+    };
+  }),
+  d3.csv("./data/csv_files/ChesterSquareSurveyResponses.csv", function(d) {
+    return {
+      visittime: d.visittime,
+      safetylevel: +d.safetylevel,
+      hour: parseInt(d.hour, 10)
+    };
+  }),
+  d3.json("./data/json_files/boston.geojson")
+];
 
-const filterBar = (d) => {
-  var result = {}
-  var rArray = []
+const filterBar = d => {
+  var result = {};
+  var rArray = [];
   var categorys = new Set(d.map(obj => obj.category));
   var neighborhoods = new Set(d.map(obj => obj.neighborhood));
   neighborhoods.forEach(n => {
-    row = {}
+    row = {};
     categorys.forEach(ot => {
-      row[ot] = 0
-    })
-    result[n] = row
-  })
+      row[ot] = 0;
+    });
+    result[n] = row;
+  });
   d.forEach(obj => {
-    result[obj.neighborhood][obj.category] = result[obj.neighborhood][obj.category] + obj.value
-  })
+    result[obj.neighborhood][obj.category] =
+      result[obj.neighborhood][obj.category] + obj.value;
+  });
   Object.keys(result).forEach(neighborhood => {
-    row = result[neighborhood]
-    filtered_row = {}
-    sortedKeys = Object.keys(result[neighborhood]).sort((a,b) => {
-      return result[neighborhood][b] - result[neighborhood][a]}
-      )
-      var max = 0
-      if(state.view !== "demographic") {
-        max = 5
-      } else {
-        max = sortedKeys.length
-      }
-    for(let i = 0; i < max; i++) {
-      filtered_row[sortedKeys[i]] = row[sortedKeys[i]]
+    row = result[neighborhood];
+    filtered_row = {};
+    sortedKeys = Object.keys(result[neighborhood]).sort((a, b) => {
+      return result[neighborhood][b] - result[neighborhood][a];
+    });
+    var max = 0;
+    if (state.view !== "demographic") {
+      max = 5;
+    } else {
+      max = sortedKeys.length;
     }
-    filtered_row["neighborhood"] = neighborhood
-    rArray.push(filtered_row)
-  })
-  return rArray
-}
+    for (let i = 0; i < max; i++) {
+      filtered_row[sortedKeys[i]] = row[sortedKeys[i]];
+    }
+    filtered_row["neighborhood"] = neighborhood;
+    rArray.push(filtered_row);
+  });
+  return rArray;
+};
 
-const filterCsBar = (d) => {
-  var result = {}
-  var rArray = []
+const filterCsBar = d => {
+  var result = {};
+  var rArray = [];
   var categorys = new Set(d.map(obj => obj.category));
   var neighborhoods = new Set(d.map(obj => obj.neighborhood));
   neighborhoods.forEach(n => {
-    row = {}
+    row = {};
     categorys.forEach(ot => {
-      row[ot] = 0
-    })
-    result[n] = row
-  })
+      row[ot] = 0;
+    });
+    result[n] = row;
+  });
   d.forEach(obj => {
-    result[obj.neighborhood][obj.category] = result[obj.neighborhood][obj.category] + obj.value
-  })
+    result[obj.neighborhood][obj.category] =
+      result[obj.neighborhood][obj.category] + obj.value;
+  });
   Object.keys(result).forEach(neighborhood => {
-    row = result[neighborhood]
-    filtered_row = {}
-    sortedKeys = Object.keys(result[neighborhood]).sort((a,b) => {
-      return result[neighborhood][b] - result[neighborhood][a]}
-      )
-    for(let i = 0; i < 10; i++) {
-      filtered_row[sortedKeys[i]] = row[sortedKeys[i]]
+    row = result[neighborhood];
+    filtered_row = {};
+    sortedKeys = Object.keys(result[neighborhood]).sort((a, b) => {
+      return result[neighborhood][b] - result[neighborhood][a];
+    });
+    for (let i = 0; i < 10; i++) {
+      filtered_row[sortedKeys[i]] = row[sortedKeys[i]];
     }
-    filtered_row["neighborhood"] = neighborhood
-    rArray.push(filtered_row)
-  })
-  return rArray.filter(a => a.neighborhood == "Chester Square")
-}
+    filtered_row["neighborhood"] = neighborhood;
+    rArray.push(filtered_row);
+  });
+  return rArray.filter(a => a.neighborhood == "Chester Square");
+};
 
-const transformSurvey = (d) => {
-  var result = {}
-  for(let i = 0; i < 23; i++) {
-    result[i] = {"total":0,"records":0}
+const transformSurvey = d => {
+  var result = {};
+  for (let i = 0; i < 23; i++) {
+    result[i] = { total: 0, records: 0 };
   }
   d.forEach(row => {
-      result[row.hour]["total"] += row.safetylevel 
-      result[row.hour]["records"] += 1
-    })
-    var final = [];
+    result[row.hour]["total"] += row.safetylevel;
+    result[row.hour]["records"] += 1;
+  });
+  var final = [];
   Object.keys(result).forEach(key => {
-    current = {}
-    current["key"] = parseInt(key)
-    if(parseInt(key) <= 7) {
-      current["value"] = result["7"]["total"]/result["7"]["records"]
+    current = {};
+    current["key"] = parseInt(key);
+    if (parseInt(key) <= 7) {
+      current["value"] = result["7"]["total"] / result["7"]["records"];
     } else if (parseInt(key) <= 14) {
-      current["value"] = result["14"]["total"]/result["14"]["records"]
+      current["value"] = result["14"]["total"] / result["14"]["records"];
     } else {
-      current["value"] = result["21"]["total"]/result["21"]["records"]
+      current["value"] = result["21"]["total"] / result["21"]["records"];
     }
-    final.push(current)
-  })
+    final.push(current);
+  });
   return final;
-}
+};
 
-const filterLine = (d) => {
-  if(state.neighborhood.length == 0) {
+const filterLine = d => {
+  if (state.neighborhood.length == 0) {
     d = d;
   } else {
-    d = d.filter(obj => state.neighborhood.includes(obj.neighborhood))
+    d = d.filter(obj => state.neighborhood.includes(obj.neighborhood));
   }
-  if(state.view == "demographic") {
-    return d.filter(obj => obj.category == "Bachelor's Degree or Higher")
+  if (state.view == "demographic") {
+    return d.filter(obj => obj.category == "Bachelor's Degree or Higher");
   } else if (state.view === "real_estate") {
-    return d.map((item) => {
-            return {"time":new Date(Date.parse(item.time)).getFullYear(),"value": item.value, "neighborhood": item.neighborhood}
-          }).sort((a,b) => a.time - b.time);
- } 
- else {
-  return d.sort((a,b) => a.category - b.category)
- }
-}
+    return d
+      .map(item => {
+        return {
+          time: new Date(Date.parse(item.time)).getFullYear(),
+          value: item.value,
+          neighborhood: item.neighborhood
+        };
+      })
+      .sort((a, b) => a.time - b.time);
+  } else {
+    return d.sort((a, b) => a.category - b.category);
+  }
+};
 
 /**
  * hi there.
  */
 const render = () => {
-  
-    lineChart(filterLine(state.data[state.view]));
-    stackChart(filterBar(state.data[state.view]));
-    geoViz({"data":state.data[state.view],"info":state.data["geo"]});
-    tablevis(filterLine(state.data[state.view]),state.data[state.view].filter(a => a.neighborhood == "Chester Square").map(s => s.value), new Set(state.data[state.view].map(s => s.neighborhood)));
-    csTopFive(filterCsBar(state.data["crime"])[0]);
-    scatterplot(state.data["survey"])
-    csOverTime({"survey":state.data["survey"],"actual":filterLine(state.data["crime"]).filter(a => a.neighborhood == "Chester Square"),"perceived":transformSurvey(state.data["survey"])})
-}
+  const linedata = filterLine(state.data[state.view]);
+  lineChart(linedata);
+  stackChart(filterBar(state.data[state.view]));
+  geoViz({ data: state.data[state.view], info: state.data["geo"] });
+  tablevis(
+    linedata,
+    state.data[state.view]
+      .filter(a => a.neighborhood == "Chester Square")
+      .map(s => s.value),
+    new Set(state.data[state.view].map(s => s.neighborhood))
+  );
+  csTopFive(filterCsBar(state.data["crime"])[0]);
+  scatterplot(state.data["survey"]);
+  csOverTime({
+    survey: state.data["survey"],
+    actual: linedata.filter(
+      a => a.neighborhood == "Chester Square"
+    ),
+    perceived: transformSurvey(state.data["survey"])
+  });
+};
 
 const updateViz = () => {
+  const linedata = filterLine(state.data[state.view]);
   removeChart();
-  lineChart(filterLine(state.data[state.view]));
+  lineChart(linedata);
   stackChart(filterBar(state.data[state.view]));
-  geoViz({"data":state.data[state.view],"info":state.data["geo"]});
-  tablevis(filterLine(state.data[state.view]),state.data[state.view].filter(a => a.neighborhood == "Chester Square").map(s => s.value), new Set(state.data[state.view].map(s => s.neighborhood)));
-}
+  geoViz({ data: state.data[state.view], info: state.data["geo"] });
+  tablevis(
+    linedata,
+    state.data[state.view]
+      .filter(a => a.neighborhood == "Chester Square")
+      .map(s => s.value),
+    new Set(state.data[state.view].map(s => s.neighborhood))
+  );
+};
 
-const setData = (d) => {
+const setData = d => {
   state.data["crime"] = d[0];
   state.data["real_estate"] = d[1];
   state.data["demographic"] = d[2];
   state.data["survey"] = d[3];
   state.data["geo"] = d[4];
-}
+};
 
 const load = () => {
-    Promise.all(promises).then(setData).then(render);
+  Promise.all(promises)
+    .then(setData)
+    .then(render);
 };
 
 stateBttns.forEach(btn => {
-  btn.addEventListener("click" , (e) => {
-    e.preventDefault()
+  btn.addEventListener("click", e => {
+    e.preventDefault();
     state.setView = btn.attributes["data-activity"].nodeValue;
-  })
+  });
 });
 
 load();
